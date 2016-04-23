58c48d8a9eff6017045945a09fdcbaf59cd9418e
Nicolas Camenisch <garnele007@gmail.com>
2016-04-23 11:03:03 +0200

    Added OS X example application

---
+++ example/OS X/SwiftOCR Example OS X/SwiftOCR Example OS X/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftOCR Example OS X
+//
+//  Created by Nicolas Camenisch on 23.04.16.
+//  Copyright © 2016 Nicolas Camenisch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ example/OS X/SwiftOCR Example OS X/SwiftOCR Example OS X/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  SwiftOCR Example OS X
+//
+//  Created by Nicolas Camenisch on 23.04.16.
+//  Copyright © 2016 Nicolas Camenisch. All rights reserved.
+//
+
+import Cocoa
+import SwiftOCR
+
+class ViewController: NSViewController {
+    
+    @IBOutlet weak var imageView: NSImageView!
+    @IBOutlet weak var recognizedLabel: NSTextFieldCell!
+    
+    var inputImage:NSImage?
+    
+    let swiftOCRInstance = SwiftOCR()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    @IBAction func imageDidChange(sender: NSImageView) {
+        inputImage = sender.image
+        ocr()
+    }
+
+    func ocr() {
+        self.swiftOCRInstance.image = self.inputImage
+        self.swiftOCRInstance.recognize({recognizedString in
+            dispatch_async(dispatch_get_main_queue(), {
+                self.recognizedLabel.title = recognizedString
+            })
+        })
+    }
+
+
+}
+

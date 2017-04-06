fbd4421b90f2f5f5cf977e83f0f03233979761a5
Serhii Londar <serhii.londar@gmail.com>
2017-04-06 23:04:58 +0300

    Train My own network;

--- framework/SwiftOCR/SwiftOCR.swift
+++ framework/SwiftOCR/SwiftOCR.swift
@@ -30,7 +30,7 @@ fileprivate func >= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
 
 ///The characters the globalNetwork can recognize.
 ///It **must** be in the **same order** as the network got trained
-public var recognizableCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+public var recognizableCharacters = "0123456789+-/*="
 ///The FFNN network used for OCR
 public var globalNetwork = FFNN.fromFile(Bundle(for: SwiftOCR.self).url(forResource: "OCR-Network", withExtension: nil)!)!
 

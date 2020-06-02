895b792c7c14c7b2567385d1c6f1e03f81b1256c
Chris Coffin <me@chriscoffin.org>
2020-06-02 12:06:42 -0400

    Swift 5 fixes SwiftOCR.swift#L584,633: Replace 'UIImageOrientation' with 'UIImage.Orientation' UIImageOrientation is deprecated as of Swift 4.2

--- framework/SwiftOCR/SwiftOCR.swift
+++ framework/SwiftOCR/SwiftOCR.swift
@@ -581,7 +581,7 @@ open class SwiftOCR {
             let orientationUp = UIImage.Orientation.up
             #else
             //GPUImage is using a re-definition of the UIImageOrientation for Mac compilation
-            let orientationUp = UIImageOrientation.up
+            let orientationUp = UIImage.Orientation.up
             #endif
             
             var processedImage:OCRImage? = dodgeBlendFilter.imageFromCurrentFramebuffer(with: orientationUp)
@@ -630,7 +630,7 @@ open class SwiftOCR {
         let orientationUp = UIImage.Orientation.up
         #else
         //GPUImage is using a re-definition of the UIImageOrientation for Mac compilation
-        let orientationUp = UIImageOrientation.up
+        let orientationUp = UIImage.Orientation.up
         #endif
         
         var processedImage:OCRImage? = thresholdFilter.imageFromCurrentFramebuffer(with: orientationUp)
